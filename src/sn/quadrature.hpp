4e1cda4e3785bcbdda10ae17493050a64f994c8f
Kurt Dominesey <domink@rpi.edu>
2019-10-02 18:02:27 -0400

    add quadrature function for azimuthal symmetry

--- src/sn/quadrature.hpp
+++ src/sn/quadrature.hpp
@@ -2,17 +2,18 @@
 #define AETHER_SN_QUADRATURE_H_
 
 #include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/point.h>
 
 dealii::Quadrature<2> gauss_chebyshev(int order) {
   int num_points = 2 * order - 1;
   dealii::QGauss<1> polar(num_points);
   dealii::QGaussChebyshev<1> azimuthal(num_points);
   return dealii::Quadrature<2>(polar, azimuthal);
-};
+}
 
 template <int dim>
-dealii::Tensor<1, dim> ordinate(Point<2> coordinate) {
-  Assert(2 <= dim <= 3, dealii::ExcMessage("2D or 3D only"))
+dealii::Tensor<1, dim> ordinate(const dealii::Point<2> coordinate) {
+  Assert(2 <= dim && dim <= 3, dealii::ExcMessage("2D or 3D only"))
   dealii::Tensor<1, dim> ordinate;
   double theta = std::acos(coordinate(0));
   double phi = std::acos(coordinate(1));
@@ -21,10 +22,28 @@ dealii::Tensor<1, dim> ordinate(Point<2> coordinate) {
   if (dim == 3)
     ordinate(2) = std::sin(theta) * std::sin(phi);
   return ordinate;
-};
+}
 
-dealii::Tensor<1, 1> ordinate(dealii::Point<1> coordinate) {
+dealii::Tensor<1, 1> ordinate(const dealii::Point<1> coordinate) {
   return coordinate;
-};
+}
+
+dealii::Quadrature<2> impose_azimuthal_symmetry(
+    const dealii::Quadrature<2> &quadrature) {
+  const std::vector<dealii::Point<2> > &points = quadrature.get_points();
+  const std::vector<double> &weights = quadrature.get_weights();
+  std::vector<dealii::Point<2> > points_sym;
+  std::vector<double> weights_sym;
+  points_sym.reserve(points.size()/2);
+  weights_sym.reserve(weights.size()/2);
+  for (int n = 0; n < points.size(); ++n) {
+    if (points[n](1) > 0.5) {
+      points_sym.push_back(points[n]);
+      weights_sym.push_back(weights[n]*2);
+    }
+  }
+  dealii::Quadrature<2> quadrature_sym(points_sym, weights_sym);
+  return quadrature_sym;
+}
 
 #endif  // AETHER_SN_QUADRATURE_H_
