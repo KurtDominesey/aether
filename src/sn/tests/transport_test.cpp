8e58a6070f9b087db6680e3cce9ac94c20089166
Kurt Dominesey <domink@rpi.edu>
2019-10-02 18:03:18 -0400

    add test for transport

---
+++ src/sn/tests/transport_test.cpp
@@ -0,0 +1,39 @@
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/dofs/dof_handler.h>
+
+#include "../transport.hpp"
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(TransportTest, Void) {
+  const int dim = 1;
+  const int qdim = 1;
+  dealii::Triangulation<dim> mesh;
+  dealii::GridGenerator::subdivided_hyper_cube(mesh, 20, 0, 1);
+  // std::map<char, std::vector<double> > cross_sections;
+  // cross_sections['T'] = {0};
+  std::vector<double> cross_sections = {0};
+  dealii::DoFHandler<dim> dof_handler(mesh);
+  dealii::FE_DGQ<dim> fe(2);
+  dof_handler.distribute_dofs(fe);
+  int num_ords_qdim = 4;
+  int num_ords = std::pow(num_ords_qdim, qdim);
+  dealii::QGauss<qdim> quadrature(num_ords);
+  Transport<dim> transport(dof_handler, quadrature, cross_sections);
+  int num_dofs = dof_handler.n_dofs();
+  dealii::BlockVector<double> source(num_ords, num_dofs);
+  dealii::BlockVector<double> flux(num_ords, num_dofs);
+  for (int n = 0; n < num_ords; ++n) {
+    flux.block(n) = n;
+  }
+  transport.vmult(flux, source);
+  for (int n = 0; n < num_ords; ++n) {
+    for (int i = 0; i < num_dofs; ++i) {
+      ASSERT_DOUBLE_EQ(flux.block(0)[i], n);
+    }
+  }
+}
+
+}
