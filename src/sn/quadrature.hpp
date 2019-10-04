5baf4e9ae76800395004748a961fc11494091e3f
Kurt Dominesey <domink@rpi.edu>
2019-10-04 15:24:20 -0400

    fix method to return ordinate from quad point

--- src/sn/quadrature.hpp
+++ src/sn/quadrature.hpp
@@ -11,21 +11,25 @@ dealii::Quadrature<2> gauss_chebyshev(int order) {
   return dealii::Quadrature<2>(polar, azimuthal);
 }
 
-template <int dim>
-dealii::Tensor<1, dim> ordinate(const dealii::Point<2> coordinate) {
-  Assert(2 <= dim && dim <= 3, dealii::ExcMessage("2D or 3D only"))
-  dealii::Tensor<1, dim> ordinate;
-  double theta = std::acos(coordinate(0));
-  double phi = std::acos(coordinate(1));
-  ordinate(0) = std::cos(theta);
-  ordinate(1) = std::sin(theta) * std::cos(phi);
-  if (dim == 3)
-    ordinate(2) = std::sin(theta) * std::sin(phi);
-  return ordinate;
-}
-
-dealii::Tensor<1, 1> ordinate(const dealii::Point<1> coordinate) {
-  return coordinate;
+template <int dim, int qdim>
+dealii::Tensor<1, dim> ordinate(const dealii::Point<qdim> coordinate) {
+  dealii::Point<dim> ordinate;
+  if (qdim == 1) {
+    Assert(dim == 1, dealii::ExcInvalidState());
+    ordinate(0) = coordinate(0);
+    return ordinate;
+  }
+  else {
+    Assert(qdim == 2, dealii::ExcInvalidState());
+    Assert(2 <= dim && dim <= 3, dealii::ExcInvalidState());
+    double theta = std::acos(coordinate(0));
+    double phi = std::acos(coordinate(1));
+    ordinate(0) = std::cos(theta);
+    ordinate(1) = std::sin(theta) * std::cos(phi);
+    if (dim == 3)
+      ordinate(2) = std::sin(theta) * std::sin(phi);
+    return ordinate;
+  }
 }
 
 dealii::Quadrature<2> impose_azimuthal_symmetry(
