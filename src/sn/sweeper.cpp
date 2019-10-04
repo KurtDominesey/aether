d5ee73e745732a8711ec5d72f8abfd6c1efbc12b
Kurt Dominesey <domink@rpi.edu>
2019-10-04 15:24:38 -0400

    move template methods of sweeper into header

--- src/sn/sweeper.cpp
+++ src/sn/sweeper.cpp
@@ -8,25 +8,3 @@ void Sweeper::initialize(std::vector<int> &octant_to_global,
   dst = &dst_ref;
   src = &src_ref;    
 }
-
-template <class DOFINFO>
-void Sweeper::initialize_info(DOFINFO &info, bool face) const {
-  info.initialize_matrices(octant_to_global.size(), face);
-  info.initialize_vectors(octant_to_global.size(), face);
-}
-
-template <class DOFINFO>
-void Sweeper::assemble(const DOFINFO &info) {
-  for (int n = 0; n < info.n_matrices(); ++n) {
-    dealii::FullMatrix<double> &local_matrix = info.matrix(n).matrix;
-    dealii::Vector<double> &local_src = info.vector(n).block(0);
-    dealii::Vector<double> local_dst = local_src;
-    local_matrix.gauss_jordan();  // directly invert
-    local_matrix.vmult(local_dst, local_src);
-    dst->block(n).add(info.indices, local_dst);
-  }
-}
-
-template <class DOFINFO>
-void Sweeper::assemble(const DOFINFO &info1, const DOFINFO &info2) {}
-// do not do any global work solely on face contributions
--- src/sn/sweeper.hpp
+++ src/sn/sweeper.hpp
@@ -11,13 +11,26 @@ class Sweeper {
                   dealii::BlockVector<double> &src_ref);
 
   template <class DOFINFO>
-  void initialize_info(DOFINFO &info, bool face) const;
+  void initialize_info(DOFINFO &info, bool face) const {
+    info.initialize_matrices(octant_to_global.size(), face);
+    info.initialize_vectors(octant_to_global.size());
+  }
 
   template <class DOFINFO>
-  void assemble(const DOFINFO &info);
+  void assemble(const DOFINFO &info) {
+    for (int n = 0; n < info.n_matrices(); ++n) {
+      dealii::FullMatrix<double> local_matrix = info.matrix(n).matrix;
+      const dealii::Vector<double> &local_src = info.vector(n).block(0);
+      dealii::Vector<double> local_dst = local_src;
+      local_matrix.gauss_jordan();  // directly invert
+      local_matrix.vmult(local_dst, local_src);
+      dst->block(n).add(info.indices, local_dst);
+    }
+  }
 
   template <class DOFINFO>
-  void assemble(const DOFINFO &info1, const DOFINFO &info2);
+  void assemble(const DOFINFO &info1, const DOFINFO &info2) {}
+  // do not do any global work solely on face contributions
 
  protected:
   std::vector<int> octant_to_global;
