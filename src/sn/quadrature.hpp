3067eb18f839b8e0f0e5d3f01c8704afbe283be5
Kurt Dominesey <domink@rpi.edu>
2019-10-09 17:44:32 -0400

    refactor ordinate method (WIP, TODO 2D/3D)

--- src/sn/quadrature.hpp
+++ src/sn/quadrature.hpp
@@ -16,20 +16,19 @@ dealii::Tensor<1, dim> ordinate(const dealii::Point<qdim> coordinate) {
   dealii::Point<dim> ordinate;
   if (qdim == 1) {
     Assert(dim == 1, dealii::ExcInvalidState());
-    ordinate(0) = coordinate(0);
-    return ordinate;
+    ordinate(0) = 2 * coordinate(0) - 1;
   }
   else {
     Assert(qdim == 2, dealii::ExcInvalidState());
     Assert(2 <= dim && dim <= 3, dealii::ExcInvalidState());
+    ordinate(0) = coordinate(0);
     double theta = std::acos(coordinate(0));
     double phi = std::acos(coordinate(1));
-    ordinate(0) = std::cos(theta);
     ordinate(1) = std::sin(theta) * std::cos(phi);
     if (dim == 3)
       ordinate(2) = std::sin(theta) * std::sin(phi);
-    return ordinate;
   }
+  return ordinate;
 }
 
 dealii::Quadrature<2> impose_azimuthal_symmetry(
