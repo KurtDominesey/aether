129b388f0a546fa94c0a52070cce56b8314c3e2c
Kurt Dominesey <domink@rpi.edu>
2019-10-15 17:22:23 -0400

    create test fixture for 1D transport (no scatter)

--- src/sn/tests/transport_test.cpp
+++ src/sn/tests/transport_test.cpp
@@ -7,30 +7,45 @@
 
 namespace {
 
-TEST(TransportTest, Void) {
-  const int dim = 1;
-  const int qdim = 1;
-  dealii::Triangulation<dim> mesh;
-  dealii::GridGenerator::subdivided_hyper_cube(mesh, 10, 0, 1);
+class TransportTest1D : public ::testing::Test {
+ private:
+  dealii::Triangulation<1> mesh;
+  
+ protected:
+  void SetUp() override {
+    const int dim = 1;
+    const int qdim = 1;
+    dealii::GridGenerator::subdivided_hyper_cube(mesh, 10, 0, 1);
+    dealii::FE_DGQ<dim> fe(3);
+    dof_handler.initialize(mesh, fe);
+    int num_ords_qdim = 8;
+    int num_ords = std::pow(num_ords_qdim, qdim);
+    quadrature = dealii::QGauss<qdim>(num_ords_qdim);
+    int num_dofs = dof_handler.n_dofs();
+    source.reinit(num_ords, num_dofs);
+    flux.reinit(num_ords, num_dofs);
+    boundary_conditions.resize(
+        2, dealii::BlockVector<double>(num_ords, fe.dofs_per_cell));
+  }
+
+  dealii::Quadrature<1> quadrature;
+  dealii::DoFHandler<1> dof_handler;
+  dealii::BlockVector<double> source;
+  dealii::BlockVector<double> flux;
+  std::vector<dealii::BlockVector<double>> boundary_conditions;
+};
+
+TEST_F(TransportTest1D, Void) {
   std::vector<double> cross_sections = {0};
-  dealii::DoFHandler<dim> dof_handler(mesh);
-  dealii::FE_DGQ<dim> fe(3);
-  dof_handler.distribute_dofs(fe);
-  int num_ords_qdim = 8;
-  int num_ords = std::pow(num_ords_qdim, qdim);
-  dealii::QGauss<qdim> quadrature(num_ords);
+  int num_ords = quadrature.size();
   int num_dofs = dof_handler.n_dofs();
-  dealii::BlockVector<double> source(num_ords, num_dofs);
-  dealii::BlockVector<double> flux(num_ords, num_dofs);
-  std::vector<dealii::BlockVector<double>> boundary_conditions(
-      2, dealii::BlockVector<double>(num_ords, fe.dofs_per_cell));
   for (int n = 0; n < num_ords; ++n) {
     flux.block(n) = 0;
     source.block(n) = 0;
     for (dealii::BlockVector<double> &boundary_condition : boundary_conditions)
       boundary_condition.block(n) = n;
   }
-  Transport<dim> transport(dof_handler, quadrature, cross_sections, 
+  Transport<1> transport(dof_handler, quadrature, cross_sections, 
                            boundary_conditions);
   transport.vmult(flux, source);
   for (int n = 0; n < num_ords; ++n) {
