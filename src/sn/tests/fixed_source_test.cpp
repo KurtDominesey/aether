ef01a97e26239a3101f0cf6db4906d2ca17a4d06
Kurt Dominesey <domink@rpi.edu>
2019-11-08 12:56:29 -0500

    make two fixed source tests (with/without GS)

--- src/sn/tests/fixed_source_test.cpp
+++ src/sn/tests/fixed_source_test.cpp
@@ -14,8 +14,7 @@
 
 namespace {
 
-template <class SolverType>
-class FixedSourceTest : public ::testing::Test {
+class FixedSourceAbstractTest : public ::testing::Test {
  protected:
   void SetUp() override {
     dealii::GridGenerator::subdivided_hyper_cube(mesh, 128, -1, 1);
@@ -35,15 +34,28 @@ class FixedSourceTest : public ::testing::Test {
   std::vector<std::vector<dealii::BlockVector<double>>> boundary_conditions;
 };
 
+template <class SolverType>
+class FixedSourceTest : public FixedSourceAbstractTest {};
+
+template <class SolverType>
+class FixedSourceRelaxedTest : public FixedSourceAbstractTest {};
+
 using SolverTypes =
+    ::testing::Types< dealii::SolverRichardson<dealii::BlockVector<double>>,
+                      dealii::SolverGMRES<dealii::BlockVector<double>>,
+                      dealii::SolverFGMRES<dealii::BlockVector<double>>,
+                      dealii::SolverBicgstab<dealii::BlockVector<double>> >;
+TYPED_TEST_CASE(FixedSourceTest, SolverTypes);
+
+using RelaxedSolverTypes =
     ::testing::Types< dealii::SolverRichardson<dealii::BlockVector<double>>,
                       dealii::SolverGMRES<dealii::BlockVector<double>>,
                       dealii::SolverFGMRES<dealii::BlockVector<double>>,
                       dealii::SolverBicgstab<dealii::BlockVector<double>>,
                       dealii::SolverRelaxation<dealii::BlockVector<double>> >;
-TYPED_TEST_CASE(FixedSourceTest, SolverTypes);
+TYPED_TEST_CASE(FixedSourceRelaxedTest, RelaxedSolverTypes);
 
-TYPED_TEST(FixedSourceTest, IsotropicPureScattering) {
+TYPED_TEST(FixedSourceRelaxedTest, IsotropicPureScattering) {
   const int dim = this->dim;
   const int qdim = this-> qdim;
   const int num_groups = 2;
@@ -93,6 +105,62 @@ TYPED_TEST(FixedSourceTest, IsotropicPureScattering) {
     within_groups[g].transport.vmult(uncollided.block(g), source.block(g),
                                      false);
   solver.solve(fixed_source, flux, uncollided, fixed_source_gs);
+  // std::cout << "iterations required " 
+  //           << solver_control.last_step() 
+  //           << std::endl;
+  for (int g = 0; g < num_groups; ++g)
+    for (int i = 0; i < flux.block(g).size(); ++i)
+      ASSERT_NEAR(1, flux.block(g)[i], 1e-10);
+}
+
+TYPED_TEST(FixedSourceTest, IsotropicPureScattering) {
+  const int dim = this->dim;
+  const int qdim = this-> qdim;
+  const int num_groups = 2;
+  const int num_ords = this->quadrature.size();
+  const int num_dofs = this->dof_handler.n_dofs();
+  MomentToDiscrete<qdim> m2d(this->quadrature);
+  DiscreteToMoment<qdim> d2m(this->quadrature);
+  std::vector<WithinGroup<dim, qdim>> within_groups;
+  std::vector<std::vector<ScatteringBlock<dim>>> downscattering(num_groups);
+  std::vector<std::vector<ScatteringBlock<dim>>> upscattering(num_groups);
+  std::vector<std::vector<double>> xs_total = {{1.0}, {1.0}};
+  std::vector<std::vector<std::vector<double>>> xs_scatter = {
+       {{0.0}, {1.0}},
+       {{1.0}, {0.0}}
+  };
+  const int dofs_per_cell = this->dof_handler.get_fe().dofs_per_cell;
+  std::vector<dealii::BlockVector<double>> boundary_conditions_g(
+      2, dealii::BlockVector<double>(num_ords, dofs_per_cell));
+  std::vector<std::vector<dealii::BlockVector<double>>> 
+      boundary_conditions(num_groups, boundary_conditions_g);
+  boundary_conditions[0][0] = 1;
+  boundary_conditions[0][1] = 1;
+  boundary_conditions[1][0] = 1;
+  boundary_conditions[1][1] = 1;
+  Transport<dim, qdim> transport(this->dof_handler, this->quadrature);
+  Scattering<dim> scattering(this->dof_handler);
+  for (int g = 0; g < num_groups; ++g) {
+    TransportBlock<dim, qdim> transport_wg(transport, xs_total[g], 
+                                           boundary_conditions[g]);
+    ScatteringBlock<dim> scattering_wg(scattering, xs_scatter[g][g]);
+    within_groups.emplace_back(transport_wg, m2d, scattering_wg, d2m);
+    for (int up = g - 1; up >= 0; --up)
+      downscattering[g].emplace_back(scattering, xs_scatter[g][up]);
+    for (int down = g + 1; down < num_groups; ++down)
+      upscattering[g].emplace_back(scattering, xs_scatter[g][down]);
+  }
+  FixedSource<dim, qdim> fixed_source(
+      within_groups, downscattering, upscattering, m2d, d2m);
+  dealii::BlockVector<double> source(num_groups, num_ords*num_dofs);
+  dealii::BlockVector<double> uncollided(num_groups, num_ords*num_dofs);
+  dealii::BlockVector<double> flux(num_groups, num_ords*num_dofs);
+  dealii::SolverControl solver_control(200, 1e-10);
+  TypeParam solver(solver_control);
+  for (int g = 0; g < within_groups.size(); ++g)
+    within_groups[g].transport.vmult(uncollided.block(g), source.block(g),
+                                     false);
+  solver.solve(fixed_source, flux, uncollided, dealii::PreconditionIdentity());
   // std::cout << "iterations required " 
   //           << solver_control.last_step() 
   //           << std::endl;
