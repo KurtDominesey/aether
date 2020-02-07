0afbbeabd553a185fd1d9a6a77e579f33fc4f36e
Kurt Dominesey <domink@rpi.edu>
2020-02-07 11:59:33 -0500

    refactor multigroup collapsed cross-sections

---
+++ src/base/mgxs.cc
@@ -0,0 +1,66 @@
+#include "base/mgxs.h"
+
+namespace aether {
+
+Mgxs read_mgxs(
+    const std::string &filename, 
+    const std::string &temperature,
+    const std::vector<std::string> &materials) {
+  namespace HDF5 = dealii::HDF5;
+  HDF5::File file(filename, HDF5::File::FileAccessMode::open);
+  const int num_groups =  file.get_attribute<int>("energy_groups");
+  const int num_materials = materials.size();
+  const int num_legendre = 1;
+  std::vector<std::vector<double>> total_pivot(
+    num_materials, std::vector<double>(num_groups));
+  auto chi_pivot = total_pivot;
+  auto nu_fission_pivot = total_pivot;
+  Mgxs mgxs(num_groups, num_materials, num_legendre);
+  for (int j = 0; j < num_materials; ++j) {
+    HDF5::Group material = file.open_group(materials[j]);
+    int order = material.get_attribute<int>("order");
+    Assert(order < num_legendre, dealii::ExcInvalidState());
+    // We should check scatter_format is "legendre", but OpenMC encodes MGXS
+    // files with ASCII strings, whereas dealii::HDF5 can only read UTF8.
+    HDF5::Group library = material.open_group(temperature);
+    total_pivot[j] = library.open_dataset("total").read<std::vector<double>>();
+    if (material.get_attribute<bool>("fissionable")) {
+      chi_pivot[j] = 
+          library.open_dataset("chi").read<std::vector<double>>();
+      nu_fission_pivot[j] = 
+          library.open_dataset("nu-fission").read<std::vector<double>>();
+    }
+    HDF5::Group scatter_data = library.open_group("scatter_data");
+    std::vector<int> g_min = 
+        scatter_data.open_dataset("g_min").read<std::vector<int>>();
+    std::vector<int> g_max = 
+        scatter_data.open_dataset("g_max").read<std::vector<int>>();
+    std::vector<double> scatter_matrix =
+        scatter_data.open_dataset("scatter_matrix").read<std::vector<double>>();
+    // Also cannot read scatter_shape. Assume [G][G'][Order].
+    // Aether uses opposite notation of OpenMC, in that g (G) denotes outgoing
+    // groups, while g' (gp, G') is incoming.
+    int gg = 0;
+    for (int ga = 0; ga < num_groups; ++ga) {
+      for (int gb = g_min[ga]; gb <= g_max[ga]; ++gb) {
+        for (int ell = 0; ell <= order; ++ell, ++gg) {
+          int gp = ga;
+          int g = gb - 1;
+          mgxs.scatter[g][gp][j*num_legendre + ell] = scatter_matrix[gg];
+        }
+      }
+    }
+    AssertDimension(gg, scatter_matrix.size());
+  }
+  // Pivot cross-section data
+  for (int g = 0; g < num_groups; ++g) {
+    for (int j = 0; j < num_materials; ++j) {
+      mgxs.total[g][j] = total_pivot[j][g];
+      mgxs.chi[g][j] = chi_pivot[j][g];
+      mgxs.nu_fission[g][j] = nu_fission_pivot[j][g];
+    }
+  }
+  return mgxs;
+}
+
+}  // namespace aether
--- src/base/mgxs.h
+++ src/base/mgxs.h
@@ -5,76 +5,22 @@
 
 namespace aether {
 
-template <int num_groups, int num_mats, int num_ell>
 struct Mgxs {
-  std::array<std::array<double, num_mats>, num_groups> total;
-  std::array<std::array<double, num_mats>, num_groups> chi;
-  std::array<std::array<double, num_mats>, num_groups> nu_fission;
-  std::array<std::array<std::array<std::array<
-    double, num_ell>, num_mats>, num_groups>, num_groups> scatter;
+  Mgxs(int num_groups, int num_materials, int num_legendre)
+      : total(num_groups, std::vector<double>(num_materials)),
+        chi(num_groups, std::vector<double>(num_materials)),
+        nu_fission(num_groups, std::vector<double>(num_materials)),
+        scatter(num_groups, std::vector<std::vector<double>>(
+                num_groups, std::vector<double>(num_materials*num_legendre))) {}
+  std::vector<std::vector<double>> total;
+  std::vector<std::vector<double>> chi;
+  std::vector<std::vector<double>> nu_fission;
+  std::vector<std::vector<std::vector<double>>> scatter;
 };
 
-template <int num_groups, int num_mats, int num_ell>
-Mgxs<num_groups, num_mats, num_ell> read_mgxs(
-    std::string filename, 
-    std::string temperature,
-    std::vector<std::string> materials) {
-  namespace HDF5 = dealii::HDF5;
-  HDF5::File file(filename, HDF5::File::FileAccessMode::open);
-  AssertDimension(num_groups, file.get_attribute<int>("energy_groups"));
-  AssertDimension(num_mats, materials.size());
-  AssertDimension(num_ell, 1);
-  std::vector<std::vector<double>> total_pivot(
-    num_mats, std::vector<double>(num_groups));
-  auto chi_pivot = total_pivot;
-  auto nu_fission_pivot = total_pivot;
-  Mgxs<num_groups, num_mats, num_ell> mgxs{};
-  for (int j = 0; j < num_mats; ++j) {
-    HDF5::Group material = file.open_group(materials[j]);
-    int order = material.get_attribute<int>("order");
-    Assert(order < num_ell, dealii::ExcInvalidState());
-    // We should check scatter_format is "legendre", but OpenMC encodes MGXS
-    // files with ASCII strings, whereas dealii::HDF5 can only read UTF8.
-    HDF5::Group library = material.open_group(temperature);
-    total_pivot[j] = library.open_dataset("total").read<std::vector<double>>();
-    if (material.get_attribute<bool>("fissionable")) {
-      chi_pivot[j] = 
-          library.open_dataset("chi").read<std::vector<double>>();
-      nu_fission_pivot[j] = 
-          library.open_dataset("nu-fission").read<std::vector<double>>();
-    }
-    HDF5::Group scatter_data = library.open_group("scatter_data");
-    std::vector<int> g_min = 
-        scatter_data.open_dataset("g_min").read<std::vector<int>>();
-    std::vector<int> g_max = 
-        scatter_data.open_dataset("g_max").read<std::vector<int>>();
-    std::vector<double> scatter_matrix =
-        scatter_data.open_dataset("scatter_matrix").read<std::vector<double>>();
-    // Also cannot read scatter_shape. Assume [G][G'][Order].
-    // Aether uses opposite notation of OpenMC, in that g (G) denotes outgoing
-    // groups, while g' (gp, G') is incoming.
-    int gg = 0;
-    for (int ga = 0; ga < num_groups; ++ga) {
-      for (int gb = g_min[ga]; gb <= g_max[ga]; ++gb) {
-        for (int ell = 0; ell <= order; ++ell, ++gg) {
-          int gp = ga;
-          int g = gb - 1;
-          mgxs.scatter[g][gp][j][ell] = scatter_matrix[gg];
-        }
-      }
-    }
-    AssertDimension(gg, scatter_matrix.size());
-  }
-  // Pivot cross-section data
-  for (int g = 0; g < num_groups; ++g) {
-    for (int j = 0; j < num_mats; ++j) {
-      mgxs.total[g][j] = total_pivot[j][g];
-      mgxs.chi[g][j] = chi_pivot[j][g];
-      mgxs.nu_fission[g][j] = nu_fission_pivot[j][g];
-    }
-  }
-  return mgxs;
-}
+Mgxs read_mgxs(const std::string &filename, 
+               const std::string &temperature,
+               const std::vector<std::string> &materials);
 
 }
 
