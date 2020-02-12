3c13b0ea76d8922e3aeb9efc9b963c58901f1191
Kurt Dominesey <domink@rpi.edu>
2020-02-12 14:46:39 -0500

    make within group constructor parameters const

--- src/sn/within_group.cc
+++ src/sn/within_group.cc
@@ -3,19 +3,19 @@
 namespace aether::sn {
 
 template <int dim, int qdim>
-WithinGroup<dim, qdim>::WithinGroup(TransportBlock<dim, qdim> &transport,
-                                    MomentToDiscrete<qdim> &m2d,
-                                    ScatteringBlock<dim> &scattering,
-                                    DiscreteToMoment<qdim> &d2m)
+WithinGroup<dim, qdim>::WithinGroup(const TransportBlock<dim, qdim> &transport,
+                                    const MomentToDiscrete<qdim> &m2d,
+                                    const ScatteringBlock<dim> &scattering,
+                                    const DiscreteToMoment<qdim> &d2m)
     : transport(transport), m2d(m2d), 
       scattering(scattering), d2m(d2m) {}
 
 template <int dim, int qdim>
 WithinGroup<dim, qdim>::WithinGroup(
-    std::shared_ptr<TransportBlock<dim, qdim>> &transport_shared,
-    MomentToDiscrete<qdim> &m2d, 
-    std::shared_ptr<ScatteringBlock<dim>> &scattering_shared,
-    DiscreteToMoment<qdim> &d2m)
+    const std::shared_ptr<TransportBlock<dim, qdim>> &transport_shared,
+    const MomentToDiscrete<qdim> &m2d, 
+    const std::shared_ptr<ScatteringBlock<dim>> &scattering_shared,
+    const DiscreteToMoment<qdim> &d2m)
     : transport(*transport_shared.get()), 
       m2d(m2d), 
       scattering(*scattering_shared.get()), 
--- src/sn/within_group.h
+++ src/sn/within_group.h
@@ -11,14 +11,14 @@ namespace aether::sn {
 template <int dim, int qdim = dim == 1 ? 1 : 2>
 class WithinGroup {
  public:
-  WithinGroup(TransportBlock<dim, qdim> &transport,
-              MomentToDiscrete<qdim> &m2d,
-              ScatteringBlock<dim> &scattering,
-              DiscreteToMoment<qdim> &d2m);
-  WithinGroup(std::shared_ptr<TransportBlock<dim, qdim>> &transport_shared,
-              MomentToDiscrete<qdim> &m2d,
-              std::shared_ptr<ScatteringBlock<dim>> &scattering_shared,
-              DiscreteToMoment<qdim> &d2m);
+  WithinGroup(const TransportBlock<dim, qdim> &transport,
+              const MomentToDiscrete<qdim> &m2d,
+              const ScatteringBlock<dim> &scattering,
+              const DiscreteToMoment<qdim> &d2m);
+  WithinGroup(const std::shared_ptr<TransportBlock<dim, qdim>> &transport_shared,
+              const MomentToDiscrete<qdim> &m2d,
+              const std::shared_ptr<ScatteringBlock<dim>> &scattering_shared,
+              const DiscreteToMoment<qdim> &d2m);
   void vmult(dealii::Vector<double> &dst,
              const dealii::Vector<double> &src) const;
   void vmult(dealii::BlockVector<double> &dst,
