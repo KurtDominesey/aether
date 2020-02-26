848efc18da61709d05ae3de9384330b2a27746ea
Kurt Dominesey <domink@rpi.edu>
2020-02-26 16:59:53 -0500

    implement source sweep method for fixed source

--- src/sn/fixed_source_problem.cc
+++ src/sn/fixed_source_problem.cc
@@ -2,6 +2,14 @@
 
 namespace aether::sn {
 
+template <int dim, int qdim, class TransportType, class TransportBlockType>
+void FixedSourceProblem<dim, qdim, TransportType, TransportBlockType>::
+    sweep_source(dealii::BlockVector<double> &dst, 
+                 const dealii::BlockVector<double> &src) const {
+  for (int g = 0; g < within_groups.size(); ++g)
+    within_groups[g].transport.vmult(dst.block(g), src.block(g), false);
+}
+
 template class FixedSourceProblem<1>;
 template class FixedSourceProblem<2>;
 template class FixedSourceProblem<3>;
