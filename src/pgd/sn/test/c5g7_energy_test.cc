c12668004862469f67eb192b01c73d5e4e50a64f
Kurt Dominesey <domink@rpi.edu>
2020-02-26 17:20:41 -0500

    refactor c5g7 mms test (full order and pgd, WIP)

--- src/pgd/sn/test/c5g7_energy_test.cc
+++ src/pgd/sn/test/c5g7_energy_test.cc
@@ -31,20 +31,40 @@ class C5G7EnergyTest : public ::testing::Test {
  protected:
   static const int dim = 2;
   static const int qdim = 2;
-  static const int num_mats = 1;
+  static const int num_mats = 2;
   const int num_groups = 7;
   const int num_ell = 1;
   const double pitch = 0.63;
   const double radius = 0.54;
   dealii::Triangulation<dim> mesh;
 
+  Mgxs ReadMgxs() {
+    const std::string filename = "/mnt/c/Users/kurt/Documents/projects/aether/examples/c5g7/c5g7.h5";
+    const std::string temperature = "294K";
+    const std::vector<std::string> materials =  {"water", "uo2"};
+    AssertDimension(materials.size(), num_mats);
+    Mgxs mgxs = read_mgxs(filename, temperature, materials);
+    for (int g = 0; g < num_groups; ++g) {
+      for (int j = 0; j < num_mats; ++j)
+        mgxs.total[g][j] *= pitch / 2;
+      for (int gp = 0; gp < num_groups; ++gp) {
+        for (int j = 0; j < num_mats; ++j) {
+          mgxs.scatter[g][gp][j] *= pitch / 2;
+          std::cout << mgxs.scatter[g][gp][j] << " ";
+        }
+      }
+      std::cout << std::endl;
+    }
+    return mgxs;
+  }
+
   void MeshQuarterPincell() {
-    mesh_quarter_pincell(mesh, {radius}, pitch, {0, 0});
+    mesh_quarter_pincell(mesh, {radius}, pitch, {1, 0});
     TransformMesh();
   }
 
   void MeshEighthPincell() {
-    mesh_eighth_pincell(mesh, {radius}, pitch, {0, 0});
+    mesh_eighth_pincell(mesh, {radius}, pitch, {1, 0});
     TransformMesh();
     // (Set reflecting boundary)
     using Cell = typename dealii::Triangulation<dim>::active_cell_iterator;
@@ -70,15 +90,147 @@ class C5G7EnergyTest : public ::testing::Test {
 
   void TransformMesh() {
     dealii::GridTools::scale(2 / pitch, mesh);
-    dealii::GridTools::shift(dealii::Point<dim>(-1, -1), mesh);
+    dealii::Point<dim> center(0, 0);
+    dealii::GridTools::shift(center, mesh);
     // Reset manifolds after transformation
     mesh.set_manifold(1,
-        dealii::SphericalManifold<dim>(dealii::Point<2>(-1, -1)));
+        dealii::SphericalManifold<dim>(center));
     dealii::TransfiniteInterpolationManifold<dim> trans_manifold;
     trans_manifold.initialize(mesh);
     mesh.set_manifold(2, trans_manifold);
+    // (Set reflecting boundary)
+    using Cell = typename dealii::Triangulation<dim>::active_cell_iterator;
+    using Face = typename dealii::Triangulation<dim>::active_face_iterator;
+    for (Cell cell = mesh.begin_active(); cell != mesh.end(); ++cell) {
+      for (int f = 0; f < dealii::GeometryInfo<dim>::faces_per_cell; ++f) {
+        Face face = cell->face(f);
+        if (face->at_boundary()) {
+          face->set_boundary_id(types::reflecting_boundary_id);
+        }
+      }
+    }
     // Refine mesh
-    mesh.refine_global(2);
+    mesh.refine_global(0);
+  }
+
+  void TestFullOrder(const Mgxs &mgxs) {
+    // Create quadrature
+    const QPglc<dim, qdim> quadrature(1, 1);
+    // Create finite elements
+    dealii::FE_DGQ<dim> fe(1);
+    dealii::DoFHandler dof_handler(mesh);
+    dof_handler.distribute_dofs(fe);
+    // Create boundary conditions
+    std::vector<std::vector<dealii::BlockVector<double>>> boundary_conditions(
+        num_groups, std::vector<dealii::BlockVector<double>>(
+            0, dealii::BlockVector<double>(quadrature.size(),
+                                           fe.dofs_per_cell)));
+    // Create solution
+    using Solution = dealii::Functions::CosineFunction<dim>;
+    Solution solution;
+    dealii::Vector<double> solution_energy(num_groups);
+    for (int g = 0; g < num_groups; ++g)
+      solution_energy[g] = g + 1;
+    // Run MMS refinement cycles
+    dealii::ConvergenceTable convergence_table;
+    int num_cycles = 2;
+    double l2_errors[num_cycles][num_groups][quadrature.size()];
+    for (int cycle = 0; cycle < num_cycles; ++cycle) {
+      if (cycle > 0) {
+        mesh.refine_global();
+        dof_handler.initialize(mesh, dof_handler.get_fe());
+      }
+      // Create source
+      dealii::BlockVector<double> source(
+          num_groups, quadrature.size() * dof_handler.n_dofs());
+      for (int g = 0; g < num_groups; ++g) {
+        dealii::BlockVector<double> source_g(quadrature.size(), 
+                                             dof_handler.n_dofs());
+        for (int n = 0; n < quadrature.size(); ++n) {
+          auto grad = std::bind(
+              &Solution::gradient, solution, std::placeholders::_1, 0);
+          Streamed<dim> streamed(quadrature.ordinate(n), grad);
+          dealii::VectorTools::interpolate(
+              dof_handler, streamed, source_g.block(n));
+          source_g.block(n) *= solution_energy[g];
+        }
+        dealii::Vector<double> source_iso(dof_handler.n_dofs());
+        dealii::VectorTools::interpolate(dof_handler, solution, source_iso);
+        AssertDimension(num_mats, 1);
+        const int j = 0;
+        double collision = solution_energy[g] * mgxs.total[g][j];
+        double scattering = 0;
+        for (int gp = 0; gp < num_groups; ++gp)
+          scattering += solution_energy[gp] * mgxs.scatter[g][gp][j];
+        for (int n = 0; n < quadrature.size(); ++n) {
+          source_g.block(n).add(collision, source_iso);
+          source_g.block(n).add(-scattering, source_iso);
+        }
+        source.block(g) = source_g;
+      }
+      // Run problem
+      FixedSourceProblem<dim, qdim> problem(
+          dof_handler, quadrature, mgxs, boundary_conditions);
+      dealii::BlockVector<double> uncollided(source.get_block_indices());
+      dealii::BlockVector<double> flux(source.get_block_indices());
+      problem.sweep_source(uncollided, source);
+      dealii::SolverControl control(500, 1e-8);
+      dealii::SolverRichardson<dealii::BlockVector<double>> solver(control);
+      solver.solve(problem.fixed_source, flux, uncollided, 
+                   dealii::PreconditionIdentity());
+      // Verify solution
+      dealii::BlockVector<double> flux_g(quadrature.size(), 
+                                         dof_handler.n_dofs());
+      for (int g = 0; g < num_groups; ++g) {
+        flux_g = flux.block(g);
+        flux_g /= solution_energy[g];
+        for (int n = 0; n < quadrature.size(); ++n) {
+          dealii::Vector<double> difference_per_cell(mesh.n_active_cells());
+          dealii::VectorTools::integrate_difference(
+              dof_handler, flux_g.block(n), solution, 
+              difference_per_cell,
+              dealii::QGauss<dim>(dof_handler.get_fe().degree + 2),
+              dealii::VectorTools::L2_norm);
+          double l2_error = difference_per_cell.l2_norm();
+          l2_errors[cycle][g][n] = l2_error;
+          if (cycle > 0) {
+            double ratio = l2_errors[cycle-1][g][n] / l2_errors[cycle][g][n];
+            double l2_conv = std::log(std::abs(ratio)) / std::log(2.0);
+            EXPECT_NEAR(l2_conv, dof_handler.get_fe().degree + 1, 5e-2);
+          }
+          std::string key = "g" + std::to_string(g) + "n" + std::to_string(n);
+          convergence_table.add_value(key, l2_error);
+          convergence_table.set_scientific(key, true);
+        }
+      }
+      if (cycle == num_cycles - 1) {
+        // Output results
+        dealii::DataOut<dim> data_out;
+        data_out.attach_dof_handler(dof_handler);
+        std::vector<dealii::BlockVector<double>*> flux_groups;
+        for (int g = 0; g < num_groups; ++g) {
+          auto flux_g = new dealii::BlockVector<double>(
+              quadrature.size(), dof_handler.n_dofs());
+          flux_groups.push_back(flux_g);
+          (*flux_g) = flux.block(g);
+          for (int n = 0; n < quadrature.size(); ++n) {
+            data_out.add_data_vector(
+                flux_g->block(n), 
+                "g" + std::to_string(g) + "n" + std::to_string(n),
+                dealii::DataOut_DoFData<dealii::DoFHandler<dim>,
+                                        dim>::DataVectorType::type_dof_data);
+          }
+        }
+        data_out.build_patches();
+        std::ofstream output("flux_full_order.vtu");
+        data_out.write_vtu(output);
+        for (dealii::BlockVector<double>* flux_g : flux_groups)
+          delete flux_g;
+      }
+    }
+    convergence_table.evaluate_all_convergence_rates(
+    dealii::ConvergenceTable::RateMode::reduction_rate_log2);
+    convergence_table.write_text(std::cout);
   }
 
   void Test(const Mgxs &mgxs) {
@@ -89,7 +241,7 @@ class C5G7EnergyTest : public ::testing::Test {
       mgxs_one.scatter[0][0][j] = 1;
     }
     // Create quadrature
-    const QPglc<dim, qdim> quadrature(2, 2);
+    const QPglc<dim, qdim> quadrature(1, 2);
     // Create finite elements
     dealii::FE_DGQ<dim> fe(1);
     dealii::DoFHandler dof_handler(mesh);
@@ -97,7 +249,7 @@ class C5G7EnergyTest : public ::testing::Test {
     // Create boundary conditions
     std::vector<std::vector<dealii::BlockVector<double>>> boundary_conditions(
         1, std::vector<dealii::BlockVector<double>>(
-            1, dealii::BlockVector<double>(quadrature.size(),
+            0, dealii::BlockVector<double>(quadrature.size(),
                                            fe.dofs_per_cell)));
     // Create solutions
     using Solution = dealii::Functions::CosineFunction<dim>;
@@ -112,7 +264,7 @@ class C5G7EnergyTest : public ::testing::Test {
     }
     // Run MMS refinement cycles
     dealii::ConvergenceTable convergence_table;
-    int num_cycles = 2;
+    int num_cycles = 4;
     double l2_errors[num_cycles][quadrature.size()];
     double l2_errors_energy[num_cycles];
     for (int cycle = 0; cycle < num_cycles; ++cycle) {
@@ -130,26 +282,20 @@ class C5G7EnergyTest : public ::testing::Test {
       for (int i = 0; i < num_solutions; ++i) {
         dealii::BlockVector<double> source_spaceangle(quadrature.size(), 
                                                       dof_handler.n_dofs());
-        auto solution_value = std::bind(
-            &Solution::value, solutions_spaceangle[i], std::placeholders::_1, 
-            0);
-        auto solution_grad = std::bind(
-            &Solution::gradient, solutions_spaceangle[i], std::placeholders::_1, 
-            0);
+        auto grad = std::bind(&Solution::gradient, solutions_spaceangle[i], 
+                              std::placeholders::_1, 0);
         for (int n = 0; n < quadrature.size(); ++n) {
-          Streamed<dim> streamed(quadrature.ordinate(n), solution_grad);
+          Streamed<dim> streamed(quadrature.ordinate(n), grad);
           dealii::VectorTools::interpolate(
               dof_handler, streamed, source_spaceangle.block(n));
         }
         sources_spaceangle[i].block(0) = source_spaceangle;
+        // sources_spaceangle[i].block(0) *= 0;
         sources_energy[i] = solutions_energy[i];
         // collision and scattering
         dealii::Vector<double> source_spaceangle_iso(dof_handler.n_dofs());
         dealii::VectorTools::interpolate(
             dof_handler, solutions_spaceangle[i], source_spaceangle_iso);
-        source_spaceangle = 0;
-        for (int n = 0; n < quadrature.size(); ++n)
-          source_spaceangle.block(n) = source_spaceangle_iso;
         for (int j = 0; j < num_mats; ++j) {
           for (int g = 0; g < num_groups; ++g) {
             sources_energy[i+1+j][g] = mgxs.total[g][j] 
@@ -158,10 +304,42 @@ class C5G7EnergyTest : public ::testing::Test {
               sources_energy[i+1+num_mats+j][g] += mgxs.scatter[g][gp][j]
                                                    * solutions_energy[i][gp];
           }
+          double collision = 0;
+          double scattering = 0;
+          for (int g = 0; g < num_groups; ++g) {
+            collision += solutions_energy[i][g] * sources_energy[i+1+j][g];
+            scattering += solutions_energy[i][g] 
+                          * sources_energy[i+1+num_mats+j][g];
+          }
+          collision /= std::pow(solutions_energy[i].l2_norm(), 2);
+          scattering /= std::pow(solutions_energy[i].l2_norm(), 2);
+          std::cout << "COLLISION TRUE " << collision << std::endl;
+          std::cout << "SCATTERING TRUE " << scattering << std::endl;
+          // source_spaceangle.print(std::cout);
+          dealii::Vector<double> source_spaceangle_iso_j(source_spaceangle_iso);
+          std::vector<dealii::types::global_dof_index> dof_indices(
+              fe.dofs_per_cell);
+          for (auto cell = dof_handler.begin_active();
+               cell != dof_handler.end(); ++cell) {
+            if (cell->material_id() != j) {
+              cell->get_dof_indices(dof_indices);
+              for (auto index : dof_indices) {
+                source_spaceangle_iso_j[index] = 0;
+              }
+            }
+          }
+          source_spaceangle = 0;
+          for (int n = 0; n < quadrature.size(); ++n)
+            source_spaceangle.block(n) = source_spaceangle_iso_j;
           sources_spaceangle[i+1+j].block(0) = source_spaceangle;
           sources_spaceangle[i+1+num_mats+j].block(0) = source_spaceangle;
           sources_spaceangle[i+1+num_mats+j].block(0) *= -1;
         }
+      }
+      double factor = solutions_energy[0].l2_norm();
+      for (int s = 0; s < num_sources; ++s) {
+        sources_energy[s] /= factor;
+        sources_spaceangle[s] *= factor;
       }
       using TransportType = pgd::sn::Transport<dim, qdim>;
       using TransportBlockType = pgd::sn::TransportBlock<dim, qdim>;
@@ -175,13 +353,15 @@ class C5G7EnergyTest : public ::testing::Test {
       pgd::sn::NonlinearGS nonlinear_gs(
           linear_ops, num_mats, num_ell, num_sources);
       nonlinear_gs.enrich();
-      for (int k = 0; k < 20; ++k) {
+      for (int k = 0; k < 30; ++k) {
         nonlinear_gs.step(dealii::BlockVector<double>(),
                           dealii::BlockVector<double>());
       }
       // Verify energetic mode
       double scaling = solutions_energy[0].l2_norm();
       energy_mg.modes.back() *= scaling;
+      std::cout << "FINAL ENERGY MODE\n";
+      energy_mg.modes.back().print(std::cout);
       for (int g = 0; g < num_groups; ++g)
         EXPECT_NEAR(energy_mg.modes.back()[g], solutions_energy[0][g], 
                     solutions_energy[0][g] * 5e-2);
@@ -235,23 +415,22 @@ class C5G7EnergyTest : public ::testing::Test {
     }
     convergence_table.evaluate_all_convergence_rates(
       dealii::ConvergenceTable::RateMode::reduction_rate_log2);
-    convergence_table.write_text(std::cout);
+    std::ofstream table_out("convergence.txt");
+    convergence_table.write_text(table_out);
+    convergence_table.write_tex(std::cout);
   }
 };
 
-TEST_F(C5G7EnergyTest, Mms) {
-  // Read materials
-  const std::string filename = "/mnt/c/Users/kurt/Documents/projects/aether/examples/c5g7/c5g7.h5";
-  const std::string temperature = "294K";
-  const std::vector<std::string> materials = {"uo2"}; //{"water", "uo2"};
-  AssertDimension(materials.size(), num_mats);
-  Mgxs mgxs = read_mgxs(filename, temperature, materials);
-  for (int g = 0; g < num_groups; ++g)
-    for (int gp = 0; gp < num_groups; ++gp)
-      for (int j = 0; j < num_mats; ++j)
-        mgxs.scatter[g][gp][j] = 0;
+TEST_F(C5G7EnergyTest, MmsPgd) {
+  Mgxs mgxs = ReadMgxs();
   MeshQuarterPincell();
   Test(mgxs);
+}
+
+TEST_F(C5G7EnergyTest, MmsFull) {
+  Mgxs mgxs = ReadMgxs();
+  MeshQuarterPincell();
+  TestFullOrder(mgxs);
 }
 
 }  // namespace
