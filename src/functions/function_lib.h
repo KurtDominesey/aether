ca2ce9b8487aa61176f0a2d8e6cd75576d5ea143
Kurt Dominesey <domink@rpi.edu>
2020-03-04 11:45:13 -0500

    add cosine function with variable period

--- src/functions/function_lib.h
+++ src/functions/function_lib.h
@@ -36,4 +36,55 @@ class Collided : public dealii::Function<dim> {
   const ScalarFunction<dim> flux;
 };
 
+template <int dim>
+class CosineFunction : public dealii::Function<dim> {
+ public:
+  CosineFunction(double period) : period(period) {}
+  double value(const dealii::Point<dim> &p, const unsigned int) const {
+    switch (dim) {
+      case 1:
+        return std::cos(period * p(0));
+      case 2:
+        return std::cos(period * p(0)) *
+               std::cos(period * p(1));
+      case 3:
+        return std::cos(period * p(0)) *
+               std::cos(period * p(1)) *
+               std::cos(period * p(2));
+      default:
+        Assert(false, ExcNotImplemented());
+      }
+    return 0.0;
+  };
+  dealii::Tensor<1, dim> gradient(const dealii::Point<dim> &p, 
+                                  const unsigned int) const {
+    dealii::Tensor<1, dim> result;
+    switch (dim) {
+      case 1:
+        result[0] = -period * std::sin(period * p(0));
+        break;
+      case 2:
+        result[0] = -period * std::sin(period * p(0)) * std::cos(period * p(1));
+        result[1] = -period * std::cos(period * p(0)) * std::sin(period * p(1));
+        break;
+      case 3:
+        result[0] = -period * std::sin(period * p(0)) *
+                     std::cos(period * p(1)) *
+                     std::cos(period * p(2));
+        result[1] = -period * std::cos(period * p(0)) *
+                     std::sin(period * p(1)) *
+                     std::cos(period * p(2));
+        result[2] = -period * std::cos(period * p(0)) *
+                     std::cos(period * p(1)) *
+                     std::sin(period * p(2));
+        break;
+      default:
+        Assert(false, ExcNotImplemented());
+    }
+    return result;
+  }
+ protected:
+  double period;
+};
+
 #endif  // AETHER_FUCTIONS_FUCTION_LIB_H_
