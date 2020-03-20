80e8defc59430b5662e039206286f77ce7c07949
Kurt Dominesey <domink@rpi.edu>
2020-03-20 15:41:30 -0400

    implement update of modes in gs, interface

--- src/pgd/sn/nonlinear_gs.cc
+++ src/pgd/sn/nonlinear_gs.cc
@@ -7,13 +7,16 @@ NonlinearGS::NonlinearGS(std::vector<LinearInterface*> &linear_ops,
     : linear_ops(linear_ops),
       inner_products_x(linear_ops.size()),
       inner_products_b(linear_ops.size(), std::vector<double>(num_sources)),
+      inner_products_all_x(linear_ops.size()),
+      inner_products_all_b(linear_ops.size()),
       inner_products_one(num_materials, num_legendre) {
   inner_products_one = 1;
 }
 
-void NonlinearGS::step(dealii::BlockVector<double> x, 
+double NonlinearGS::step(dealii::BlockVector<double> x, 
                        const dealii::BlockVector<double> b,
                        const bool should_normalize) {
+  double res_max = 0;
   std::vector<InnerProducts> coefficients_x(inner_products_x[0].size(),
                                             inner_products_x[0].back());
   std::vector<double> coefficients_b(inner_products_b[0].size());
@@ -30,11 +33,14 @@ void NonlinearGS::step(dealii::BlockVector<double> x,
       for (int n = 0; n < coefficients_b.size(); ++n)
         coefficients_b[n] *= inner_products_b[j][n];
     }
+    double res = linear_ops[i]->get_residual(coefficients_x, coefficients_b);
+    res_max = std::max(res, res_max);
     linear_ops[i]->step(x, b, coefficients_x, coefficients_b);
     if (should_normalize)
       linear_ops[i]->normalize();
     linear_ops[i]->get_inner_products(inner_products_x[i], inner_products_b[i]);
   }
+  return res_max;
 }
 
 void NonlinearGS::enrich() {
@@ -52,6 +58,54 @@ void NonlinearGS::enrich() {
 void NonlinearGS::set_inner_products() {
   for (int i = 0; i < linear_ops.size(); ++i)
     linear_ops[i]->get_inner_products(inner_products_x[i], inner_products_b[i]);
+}
+
+void NonlinearGS::finalize() {
+  const int num_modes = inner_products_x[0].size();
+  std::cout << "num_modes " << num_modes << std::endl;
+  for (int i = 0; i < linear_ops.size(); ++i) {
+    inner_products_all_x[i].push_back(inner_products_x[i]);
+    inner_products_all_b[i].push_back(inner_products_b[i]);
+    AssertDimension(inner_products_all_x[i].size(), num_modes);
+    AssertDimension(inner_products_all_b[i].size(), num_modes);
+    for (int m_row = 0; m_row < num_modes - 1; ++m_row) {
+      inner_products_all_x[i][m_row].push_back(inner_products_one);
+      std::cout << inner_products_all_x[i][m_row].size() << std::endl;
+      AssertDimension(inner_products_all_x[i][m_row].size(), num_modes);
+      linear_ops[i]->get_inner_products(
+          inner_products_all_x[i][m_row], inner_products_all_b[i][m_row],
+          m_row, num_modes-1);
+    }
+  }
+}
+
+void NonlinearGS::update() {
+  const int num_modes = inner_products_x[0].size();
+  const int num_sources = inner_products_b[0].size();
+  for (int i = 0; i < linear_ops.size(); ++i) {
+    auto updatable = dynamic_cast<LinearUpdatableInterface*>(linear_ops[i]);
+    if (updatable == NULL)
+      continue;
+    std::vector<std::vector<InnerProducts>> coefficients_x(
+        num_modes, std::vector<InnerProducts>(num_modes, inner_products_one));
+    std::vector<std::vector<double>> coefficients_b(
+        num_modes, std::vector<double>(num_sources, 1.0));
+    for (int j = 0; j < linear_ops.size(); ++j) {
+      if (i == j)
+        continue;
+      for (int m_row = 0; m_row < num_modes; ++m_row) {
+        for (int m_col = 0; m_col < num_modes; ++m_col)
+          coefficients_x[m_row][m_col] *= inner_products_all_x[j][m_row][m_col];
+        for (int s = 0; s < num_sources; ++s)
+          coefficients_b[m_row][s] *= inner_products_all_b[j][m_row][s];
+      }
+    }
+    updatable->update(coefficients_x, coefficients_b);
+  }
+}
+
+void NonlinearGS::reweight() {
+  
 }
 
 }  // namespace aether::pgd::sn
--- src/pgd/sn/nonlinear_gs.h
+++ src/pgd/sn/nonlinear_gs.h
@@ -5,6 +5,7 @@
 #include <deal.II/lac/block_vector.h>
 
 #include "pgd/sn/linear_interface.h"
+#include "pgd/sn/linear_updatable_interface.h"
 #include "pgd/sn/inner_products.h"
 
 namespace aether::pgd::sn {
@@ -13,13 +14,20 @@ class NonlinearGS {
  public:
   NonlinearGS(std::vector<LinearInterface*> &linear_ops, 
               int num_materials, int num_legendre, int num_sources);
-  void step(dealii::BlockVector<double> x, 
-            const dealii::BlockVector<double> b,
-            const bool should_normalize = true);
+  double step(dealii::BlockVector<double> x, 
+              const dealii::BlockVector<double> b,
+              const bool should_normalize = true);
+  void update();
+  void reweight();
+  void vmult(dealii::BlockVector<double> dst,
+             const dealii::BlockVector<double> src);
   void enrich();
   void set_inner_products();
+  void finalize();
   std::vector<std::vector<InnerProducts>> inner_products_x;
   std::vector<std::vector<double>> inner_products_b;
+  std::vector<std::vector<std::vector<InnerProducts>>> inner_products_all_x;
+  std::vector<std::vector<std::vector<double>>> inner_products_all_b;
  protected:
   std::vector<LinearInterface*> &linear_ops;
   InnerProducts inner_products_one;
