22ccd72c868475393ad896e8b68dafc4125595a7
Kurt Dominesey <domink@rpi.edu>
2020-05-01 16:15:47 -0400

    add function to test symmetric quarter pin-cell

--- src/mesh/tests/pincell_test.cc
+++ src/mesh/tests/pincell_test.cc
@@ -18,6 +18,16 @@ TEST(MeshTest, QuarterPincell) {
   grid_out.write_svg(triangulation, file);
 }
 
+TEST(MeshTest, SymmetricQuarterPincell) {
+  dealii::Triangulation<2> triangulation;
+  mesh_symmetric_quarter_pincell(triangulation, {0.44, 0.54}, 0.63, {1, 1, 0});
+  triangulation.refine_global(2);
+  dealii::GridOut grid_out;
+  std::string filename = "symmetric_quarter_pincell.svg";
+  std::ofstream file(filename);
+  grid_out.write_svg(triangulation, file);
+}
+
 TEST(MeshTest, EighthPincell) {
   dealii::Triangulation<2> triangulation;
   mesh_eighth_pincell_ul(triangulation, {0.44, 0.54}, 0.63, {1, 1, 0});
