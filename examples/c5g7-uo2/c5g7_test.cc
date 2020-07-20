2fbddb5eaa57af32aa92a29abe69fae2aa92c91d
Kurt Dominesey <domink@rpi.edu>
2020-07-20 12:40:13 -0400

    update c5g7 test (mgxs, quadrature, mesh)

--- examples/c5g7-uo2/c5g7_test.cc
+++ examples/c5g7-uo2/c5g7_test.cc
@@ -12,10 +12,10 @@ class C5G7Test : virtual public ExampleTest<dim_, qdim_> {
 
   void SetUp() override {
     const std::vector<std::string> materials =  {"water", "uo2"};
-    mgxs = std::make_shared<Mgxs>(7, materials.size(), 1);
-    read_mgxs(*mgxs, "c5g7.h5", "294K", materials);
-    quadrature = QPglc<qdim_>(1, 2);
-    mesh_quarter_pincell(mesh, {0.54}, pitch, {0, 1});
+    mgxs = std::make_unique<Mgxs>(7, materials.size(), 1);
+    read_mgxs(*mgxs, "c5g7.h5", "294K", materials, true);
+    quadrature = QPglc<qdim_>(4, 6);
+    mesh_symmetric_quarter_pincell(mesh, {0.54}, pitch, {0, 1});
     set_all_boundaries_reflecting(mesh);
     mesh.refine_global(0);
   }
