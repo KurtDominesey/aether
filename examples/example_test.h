b6a8e31018cbd628033fcf65724107f7d5e170a0
Kurt Dominesey <domink@rpi.edu>
2020-07-20 11:36:46 -0400

    add methods to plot fluxes and differences

--- examples/example_test.h
+++ examples/example_test.h
@@ -78,6 +78,100 @@ class ExampleTest : public ::testing::Test {
     table.write_text(out);
     out.close();
   }
+
+  void PlotFlux(const dealii::BlockVector<double> &flux,
+                const DiscreteToMoment<dim, qdim> &d2m,
+                const std::vector<double> &group_structure,
+                const std::string &suffix="") const {
+    dealii::DataOut<dim> data_out;
+    data_out.attach_dof_handler(this->dof_handler);
+    const int num_groups = flux.n_blocks();
+    const int digits = std::to_string(num_groups).size();
+    AssertDimension(group_structure.size(), num_groups+1);
+    dealii::BlockVector<double> flux_m(num_groups, this->dof_handler.n_dofs());
+    for (int g = 0; g < num_groups; ++g) {
+      const std::string gs = std::to_string(g+1);
+      d2m.vmult(flux_m.block(g), flux.block(g));
+      if (!group_structure.empty()) {
+        int g_rev = num_groups - 1 - g;
+        flux_m.block(g) /=
+            std::log(group_structure[g_rev+1] / group_structure[g_rev]);
+      }
+      data_out.add_data_vector(
+          flux_m.block(g), "g" + std::string(digits - gs.size(), '0') + gs,
+          dealii::DataOut_DoFData<dealii::DoFHandler<dim>,
+                                  dim>::DataVectorType::type_dof_data);
+    }
+    data_out.build_patches();
+    std::string name = GetTestName();
+    if (!suffix.empty())
+      name += "-" + suffix;
+    std::ofstream output(name+".vtu");
+    data_out.write_vtu(output);
+  }
+
+  void PlotDiffAngular(const dealii::BlockVector<double> &flux,
+                       const dealii::BlockVector<double> &ref,
+                       const DiscreteToMoment<dim, qdim> &d2m,
+                       const std::string &suffix="") {
+    dealii::DataOut<dim> data_out;
+    data_out.attach_dof_handler(this->dof_handler);
+    const int num_groups = flux.n_blocks();
+    const int digits = std::to_string(num_groups).size();
+    dealii::BlockVector<double> diff_m(num_groups, this->dof_handler.n_dofs());
+    dealii::Vector<double> diff_g(
+        this->quadrature.size()*this->dof_handler.n_dofs());
+    dealii::Vector<double> ref_m(this->dof_handler.n_dofs());
+    for (int g = 0; g < num_groups; ++g) {
+      const std::string gs = std::to_string(g+1);
+      diff_g = flux.block(g);
+      diff_g -= ref.block(g);
+      d2m.vmult(diff_m.block(g), diff_g);
+      d2m.vmult(ref_m, ref.block(g));
+      for (int i = 0; i < diff_m.block(g).size(); ++i)
+        diff_m.block(g)[i] /= ref_m[i];
+      data_out.add_data_vector(
+          diff_m.block(g), "g" + std::string(digits - gs.size(), '0') + gs,
+          dealii::DataOut_DoFData<dealii::DoFHandler<dim>,
+                                  dim>::DataVectorType::type_dof_data);
+    }
+    data_out.build_patches();
+    std::string name = GetTestName();
+    if (!suffix.empty())
+      name += "-" + suffix;
+    std::ofstream output(name+".vtu");
+    data_out.write_vtu(output);
+  }
+
+  void PlotDiffScalar(const dealii::BlockVector<double> &flux,
+                      const dealii::BlockVector<double> &ref,
+                      const DiscreteToMoment<dim, qdim> &d2m,
+                      const std::string &suffix="") {
+    dealii::DataOut<dim> data_out;
+    data_out.attach_dof_handler(this->dof_handler);
+    const int num_groups = flux.n_blocks();
+    const int digits = std::to_string(num_groups).size();
+    dealii::BlockVector<double> diff_m(num_groups, this->dof_handler.n_dofs());
+    dealii::Vector<double> ref_g(this->dof_handler.n_dofs());
+    for (int g = 0; g < num_groups; ++g) {
+      const std::string gs = std::to_string(g+1);
+      d2m.vmult(ref_g, ref.block(g));
+      d2m.vmult(diff_m.block(g), flux.block(g));
+      diff_m.block(g) -= ref_g;
+      for (int i = 0; i < diff_m.block(g).size(); ++i)
+        diff_m.block(g)[i] /= ref_g[i];
+      data_out.add_data_vector(
+          diff_m.block(g), "g" + std::string(digits - gs.size(), '0') + gs,
+          dealii::DataOut_DoFData<dealii::DoFHandler<dim>,
+                                  dim>::DataVectorType::type_dof_data);
+    }
+    data_out.build_patches();
+    std::string name = GetTestName();
+    if (!suffix.empty())
+      name += "-" + suffix;
+    std::ofstream output(name+".vtu");
+    data_out.write_vtu(output);
+  }
 };
 
 #endif  // AETHER_EXAMPLES_EXAMPLE_TEST_H_
