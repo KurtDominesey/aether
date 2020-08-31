b1cb8ff0ba44a46c8bf01ebdb8216e9909193adf
Kurt Dominesey <domink@rpi.edu>
2020-08-31 13:02:07 -0400

    parameterize by pattern & set unequal powers

--- examples/cathalau-uo2/checkerboard_test.cc
+++ examples/cathalau-uo2/checkerboard_test.cc
@@ -12,8 +12,14 @@ namespace cathalau {
 using namespace aether;
 using namespace aether::sn;
 
+enum Pattern {
+  CHECKERBOARD,
+  STRIPES
+};
+
 class CheckerboardTest : public CathalauTest,
-                         public CoarseTest<dim_, qdim_> {
+                         public CoarseTest<dim_, qdim_>,
+                         public ::testing::WithParamInterface<Pattern> {
  protected:
   std::vector<std::string> materials_uo2;
   std::vector<std::string> materials_mox;
@@ -40,6 +46,7 @@ class CheckerboardTest : public CathalauTest,
       for (int j = 0; j < materials_uo2.size(); ++j) {
         mgxs->total[g][j] = mgxs_uo2.total[g][j];
         mgxs->chi[g][j] = mgxs_uo2.chi[g][j];
+        mgxs->nu_fission[g][j] = mgxs_uo2.nu_fission[g][j];
         for (int gp = 0; gp < num_groups; ++gp)
           mgxs->scatter[g][gp][j] = mgxs_uo2.scatter[g][gp][j];
       }
@@ -47,6 +54,7 @@ class CheckerboardTest : public CathalauTest,
         int jj = materials_uo2.size() + j;
         mgxs->total[g][jj] = mgxs_mox.total[g][j];
         mgxs->chi[g][jj] = mgxs_mox.chi[g][j];
+        mgxs->nu_fission[g][jj] = mgxs_mox.nu_fission[g][j];
         for (int gp = 0; gp < num_groups; ++gp)
           mgxs->scatter[g][gp][jj] = mgxs_mox.scatter[g][gp][j];
       }
@@ -55,18 +63,29 @@ class CheckerboardTest : public CathalauTest,
       mgxs->group_structure[g] = mgxs_uo2.group_structure[g];
     quadrature = QPglc<qdim_>(4, 8);
     AssertDimension(regions.size(), radii.size()+1);
+    std::vector<int> regions_mox = regions;
+    for (int r = 0; r < regions_mox.size(); ++r)
+      regions_mox[r] += materials_uo2.size();
     const int mani_id_trans = 1;
     const int mani_id_left = 2;
     const int mani_id_right = 3;
     dealii::Triangulation<dim_> octant_left;
     dealii::Triangulation<dim_> octant_right;
-    mesh_symmetric_quarter_pincell(octant_left, radii, pitch, regions, 
-                                   mani_id_trans, mani_id_left);
-    std::vector<int> regions_mox = regions;
-    for (int r = 0; r < regions_mox.size(); ++r)
-      regions_mox[r] += materials_uo2.size();
-    mesh_symmetric_quarter_pincell(octant_right, radii, pitch, regions_mox,
-                                   mani_id_trans, mani_id_right);
+    const Pattern pattern = this->GetParam();
+    switch (pattern) {
+      case CHECKERBOARD:
+        mesh_eighth_pincell(octant_left, radii, pitch, regions, 
+                            mani_id_trans, mani_id_left);
+        mesh_eighth_pincell_ul(octant_right, radii, pitch, regions_mox,
+                               mani_id_trans, mani_id_right);
+        break;
+      case STRIPES:
+        mesh_symmetric_quarter_pincell(octant_left, radii, pitch, regions, 
+                                       mani_id_trans, mani_id_left);
+        mesh_symmetric_quarter_pincell(octant_right, radii, pitch, regions_mox,
+                                      mani_id_trans, mani_id_right);
+        break;
+    }
     dealii::GridTools::rotate(dealii::numbers::PI_2, octant_right);
     dealii::GridTools::shift(dealii::Point<dim_>(pitch*2, 0), octant_right);
     dealii::GridGenerator::merge_triangulations(
@@ -90,6 +109,65 @@ class CheckerboardTest : public CathalauTest,
     std::cout << "return SetUp\n";
   }
 
+  void RunInfiniteMedium(dealii::Vector<double> &spectrum,
+                         const Mgxs &mgxs, const std::vector<double> &volumes) {
+    const int num_groups = mgxs.total.size();
+    const int num_materials = mgxs.total[0].size();
+    AssertDimension(spectrum.size(), num_groups);
+    AssertDimension(volumes.size(), num_materials);
+    // Assemble linear system
+    spectrum = 0;
+    dealii::FullMatrix<double> matrix(num_groups);
+    for (int j = 0; j < num_materials; ++j) {
+      std::cout << volumes[j] << " ";
+      for (int g = 0; g < num_groups; ++g) {
+        spectrum[g] += mgxs.chi[g][j] * volumes[j];
+        matrix[g][g] += mgxs.total[g][j] * volumes[j];
+        for (int gp = 0; gp < num_groups; ++gp) {
+          matrix[g][gp] -= mgxs.scatter[g][gp][j] * volumes[j];
+        }
+      }
+    }
+    std::cout << std::endl;
+    matrix.gauss_jordan();
+    // Power iterations
+    dealii::Vector<double> emission(spectrum.size());
+    double k = 1;
+    double emitted = 0;
+    double emitted_last = 0;
+    double power = 0;
+    double power_last = 0;
+    for (int i = 0; i < 100; ++i) {
+      emission = 0;
+      power = 0;
+      for (int j = 0; j < num_materials; ++j) {
+        double power_j = 0;
+        for (int g = 0; g < num_groups; ++g)
+          power_j += mgxs.nu_fission[g][j] * spectrum[g] * volumes[j];
+        for (int g = 0; g < num_groups; ++g)
+          emission[g] += power_j * mgxs.chi[g][j];
+        power += power_j;
+        // std::cout << power << std::endl;
+      }
+      emitted_last = emitted;
+      for (int g = 0; g < num_groups; ++g)
+        emitted += emission[g];
+      // std::cout << emitted << std::endl;
+      if (i > 0) {
+        // k = (emitted / emitted_last) * k;
+        double k_last = k;
+        k = (power / power_last) * k;
+        // std::cout << std::abs(k - k_last) * 1e5 << " pcm" << std::endl;
+      }
+      power_last = power;
+      emission /= k;
+      matrix.vmult(spectrum, emission);
+      // std::cout << k << std::endl;
+    }
+    std::cout << k << std::endl;
+    return;
+  }
+
   void CompareCoarse(const int num_modes,
                      const int max_iters_nonlinear,
                      const double tol_nonlinear,
@@ -97,10 +175,34 @@ class CheckerboardTest : public CathalauTest,
                      const int max_iters_fullorder,
                      const double tol_fullorder,
                      const std::vector<int> &g_maxes,
-                     const std::vector<std::string> &materials) {
+                     const std::vector<std::string> &materials,
+                     const bool unequal_powers) {
+    std::vector<double> factors(materials.size(), 1.);
+    if (unequal_powers) {
+      // run infinite medium problem
+      std::vector<double> volumes;
+      this->SetVolumes(volumes);
+      for (int j = 0; j < materials_mox.size(); ++j)
+        volumes[j+materials_uo2.size()] = volumes[j];
+      const int num_groups = mgxs->total.size();
+      dealii::Vector<double> spectrum(num_groups);
+      RunInfiniteMedium(spectrum, *mgxs, volumes);
+      int j_uo2 = 2;
+      int j_mox = materials_uo2.size() + j_uo2;
+      double emitted_uo2 = 0;
+      double emitted_mox = 0;
+      for (int g = 0; g < num_groups; ++g) {
+        emitted_uo2 += mgxs->nu_fission[g][j_uo2] * spectrum[g] * volumes[j_uo2];
+        emitted_mox += mgxs->nu_fission[g][j_mox] * spectrum[g] * volumes[j_mox];
+      }
+      double mox_ratio = emitted_mox / emitted_uo2;
+      std::cout << "RATIO " << mox_ratio << std::endl;
+      factors[j_mox] = mox_ratio;
+      // return;
+    }
     CoarseTest<dim_, qdim_>::CompareCoarse(
         num_modes, max_iters_nonlinear, tol_nonlinear, do_update, 
-        max_iters_fullorder, tol_fullorder, g_maxes, materials);
+        max_iters_fullorder, tol_fullorder, g_maxes, materials, factors, true);
     const int num_legendre = 1;
     // const int num_groups = mgxs->total.size();
     const int num_groups_coarse = g_maxes.size();
@@ -208,15 +310,25 @@ class CheckerboardTest : public CathalauTest,
   }
 };
 
-TEST_F(CheckerboardTest, UniformFissionSource) {
-  // SHEM-361 to CASMO-70
-  const std::vector<int> g_maxes = {
-      9, 12, 14, 18, 23, 26, 30, 33, 37, 40, 43, 49, 53, 56, 59, 61, 65, 68,
-      72, 86, 113, 134, 153, 171, 201, 223, 276, 280, 288, 295, 297, 301, 306,
-      310, 312, 314, 315, 317, 318, 319, 321, 322, 323, 324, 325, 327, 330,
-      333, 334, 336, 337, 338, 339, 340, 341, 343, 345, 347, 348, 349, 350,
-      351, 352, 353, 354, 355, 357, 358, 359, 361};
-  this->CompareCoarse(50, 50, 1e-4, true, 1000, 1e-8, g_maxes, this->materials);
+// SHEM-361 to CASMO-70
+const std::vector<int> g_maxes = {
+    9, 12, 14, 18, 23, 26, 30, 33, 37, 40, 43, 49, 53, 56, 59, 61, 65, 68,
+    72, 86, 113, 134, 153, 171, 201, 223, 276, 280, 288, 295, 297, 301, 306,
+    310, 312, 314, 315, 317, 318, 319, 321, 322, 323, 324, 325, 327, 330,
+    333, 334, 336, 337, 338, 339, 340, 341, 343, 345, 347, 348, 349, 350,
+    351, 352, 353, 354, 355, 357, 358, 359, 361};
+
+TEST_P(CheckerboardTest, EqualPowers) {
+  this->CompareCoarse(50, 50, 1e-4, true, 50, 1e-8, g_maxes, this->materials, 
+                      false);
 }
+
+TEST_P(CheckerboardTest, UnequalPowers) {
+  this->CompareCoarse(50, 50, 1e-4, true, 50, 1e-8, g_maxes, this->materials, 
+                      true);
+}
+
+INSTANTIATE_TEST_CASE_P(Pattern, CheckerboardTest,
+                        ::testing::Values(CHECKERBOARD, STRIPES));
 
 }
