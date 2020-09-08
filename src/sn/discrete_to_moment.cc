b842da6c8923e9b345675fa9cd0af9146c30e3f2
Kurt Dominesey <domink@rpi.edu>
2020-09-08 12:51:03 -0400

    add convenience functions related to d2m

--- src/sn/discrete_to_moment.cc
+++ src/sn/discrete_to_moment.cc
@@ -164,4 +164,31 @@ template class DiscreteToMoment<1>;
 template class DiscreteToMoment<2>;
 template class DiscreteToMoment<3>;
 
+int num_moments(int order, int dim) {
+  switch (dim) {
+    case 1: return order + 1;
+    case 2: return (order + 1) * (order + 2) / 2;
+    case 3: return std::pow(order+1, 2);
+    default: AssertThrow(false, dealii::ExcImpossibleInDim(dim));
+  }
+}
+
+int legendre_order(int num_moments, int dim) {
+  switch (dim) {
+    case 1: return num_moments - 1;
+    case 2: return (std::sqrt(8 * num_moments + 1) - 3) / 2;
+    case 3: return std::sqrt(num_moments) - 1;
+    default: AssertThrow(false, dealii::ExcImpossibleInDim(dim));
+  }
+}
+
+int num_moments_of_order(int ell, int dim) {
+  switch (dim) {
+    case 1: return 1;
+    case 2: return ell + 1;
+    case 3: return 2 * ell + 1;
+    default: AssertThrow(false, dealii::ExcImpossibleInDim(dim));
+  }
+}
+
 }  // namespace aether::sn
