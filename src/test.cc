a4d29fc1b2266acffd1da84ccd33d6f0d7380d11
Kurt Dominesey <domink@rpi.edu>
2020-12-11 10:35:35 -0500

    don't abort on deal.ii assertions in unit tests

--- src/test.cc
+++ src/test.cc
@@ -6,5 +6,6 @@
 int main (int argc, char **argv) {
   dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
   ::testing::InitGoogleTest(&argc, argv);
+  dealii::deal_II_exceptions::disable_abort_on_exception();
   return RUN_ALL_TESTS();
 }
