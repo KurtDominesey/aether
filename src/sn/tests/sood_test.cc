03f012cb6124edd3b4784f047b91001683f1ad1c
Kurt Dominesey <domink@rpi.edu>
2020-12-15 14:31:40 -0500

    reorganize Sood test

--- src/sn/tests/sood_test.cc
+++ src/sn/tests/sood_test.cc
@@ -23,72 +23,59 @@ namespace {
 
 class SoodTest : public testing::Test {
  protected:
-  void Main() {
+  static const int dim = 1;
+  std::unique_ptr<Mgxs> mgxs;
+  dealii::Triangulation<dim> mesh;
+  dealii::DoFHandler<dim> dof_handler;
+  QAngle<dim> quadrature;
+  using BoundaryConditions = 
+      std::vector<std::vector<dealii::BlockVector<double> > >;
+  BoundaryConditions boundary_conditions;
+  std::unique_ptr<FissionProblem<dim>> problem;
+
+  void SetUp() override {
     // Pu-a cross-sections
     const int num_groups = 1;
-    Mgxs mgxs(num_groups, 1, 1);
-    mgxs.nu_fission[0][0] = 3.24 * 0.081600;
-    mgxs.scatter[0][0][0] = 0.225216;
-    mgxs.total[0][0] = 0.32640;
-    mgxs.chi[0][0] = 1;
+    mgxs = std::make_unique<Mgxs>(num_groups, 1, 1);
+    mgxs->nu_fission[0][0] = 3.24 * 0.081600;
+    mgxs->scatter[0][0][0] = 0.225216;
+    mgxs->total[0][0] = 0.32640;
+    mgxs->chi[0][0] = 1;
     const int dim = 1;
     dealii::FE_DGQ<dim> fe(1);
-    dealii::Triangulation<dim> mesh;
     const double length = 1.853722;  // cm, critical slab half-length
     dealii::GridGenerator::subdivided_hyper_cube(mesh, 50, -length, length);
-    dealii::DoFHandler<dim> dof_handler(mesh);
-    dof_handler.distribute_dofs(fe);
-    const QPglc<dim> quadrature(128);
+    dof_handler.initialize(mesh, fe);
+    quadrature = QPglc<dim>(128);
     // vacuum boundary conditions (for 1 group and 2 boundaries)
-    const std::vector<std::vector<dealii::BlockVector<double>>>
-        boundary_conditions(1, std::vector<dealii::BlockVector<double>>(2,
+    boundary_conditions.resize(num_groups, 
+        std::vector<dealii::BlockVector<double>>(dim == 1 ? 2 : 1,
           dealii::BlockVector<double>(quadrature.size(), fe.dofs_per_cell)));
-    FissionProblem<dim> problem(
-        dof_handler, quadrature, mgxs, boundary_conditions);
-    dealii::ReductionControl control(100, 1e-10, 1e-4);
-    dealii::SolverGMRES<dealii::BlockVector<double>> solver(control);
-    PETScWrappers::FissionSource fission_source(
-        problem.fixed_source, problem.fission, solver, 
-        dealii::PreconditionIdentity());
-    dealii::SolverControl control_k(100, 1e-8);
-    const int size = dof_handler.n_dofs() * quadrature.size();
-    this->Solve(fission_source, control_k, num_groups, size);
-  }
-  // this is ugly, but we can't override the Solve method if it's a template
-  using FissionSourceInst = 
-      PETScWrappers::FissionSource<1, 1, 
-        dealii::SolverGMRES<dealii::BlockVector<double>>, 
-        dealii::PreconditionIdentity>;
-  virtual void Solve(FissionSourceInst &matrix, 
-                     dealii::SolverControl &control, int num_groups, int size) {
-    dealii::GrowingVectorMemory<dealii::BlockVector<double>> memory;
-    dealii::EigenPower<dealii::BlockVector<double>> solver(control, memory);
-    double k = 0.5;  // bad initial guess
-    dealii::BlockVector<double> flux(num_groups, size);
-    flux = 1;
-    solver.solve(k, matrix, flux);
-    EXPECT_NEAR(k, 1, 1e-5);  // within one pcm of criticality
+    problem = std::make_unique<FissionProblem<dim>>(
+        dof_handler, quadrature, *mgxs, boundary_conditions);
   }
 };
 
 TEST_F(SoodTest, PuAOneGroupIsotropicSlab) {
-  this->Main();
+  const int num_groups = this->mgxs->total.size();
+  dealii::ReductionControl control_wg(100, 1e-10, 1e-4);
+  dealii::SolverGMRES<dealii::BlockVector<double>> solver_wg(control_wg);
+  FissionSource fission_source(
+      this->problem->fixed_source, this->problem->fission, solver_wg, 
+      dealii::PreconditionIdentity());
+  dealii::SolverControl control(100, 1e-8);
+  const int size = this->dof_handler.n_dofs() * this->quadrature.size();
+  dealii::GrowingVectorMemory<dealii::BlockVector<double>> memory;
+  dealii::EigenPower<dealii::BlockVector<double>> eigensolver(control, memory);
+  double k = 0.5;  // bad initial guess
+  dealii::BlockVector<double> flux(num_groups, size);
+  flux = 1;
+  eigensolver.solve(k, fission_source, flux);
+  EXPECT_NEAR(k, 1, 1e-5);  // within one pcm of criticality
 }
 
 template <typename Solver>
-class SoodSLEPcTest : public SoodTest {
- protected:
-  void Solve(FissionSourceInst &matrix, dealii::SolverControl &control, 
-             int num_groups, int size) override {
-    const int size_v = num_groups * size;
-    std::vector<dealii::PETScWrappers::MPI::Vector> eigenvectors;
-    eigenvectors.emplace_back(MPI_COMM_WORLD, size_v, size_v);
-    std::vector<double> eigenvalues = {0.5};
-    Solver eigensolver(control);
-    eigensolver.solve(matrix, eigenvalues, eigenvectors, 1);
-    EXPECT_NEAR(eigenvalues[0], 1, 1e-5);
-  }
-};
+class SoodSLEPcTest : public SoodTest {};
 
 using Solvers = ::testing::Types<
     dealii::SLEPcWrappers::SolverPower,
@@ -100,7 +87,21 @@ using Solvers = ::testing::Types<
 TYPED_TEST_CASE(SoodSLEPcTest, Solvers);
 
 TYPED_TEST(SoodSLEPcTest, PuAOneGroupIsotropicSlab) {
-  this->Main();
+  const int num_groups = this->mgxs->total.size();
+  dealii::ReductionControl control_wg(100, 1e-10, 1e-4);
+  dealii::SolverGMRES<dealii::BlockVector<double>> solver_wg(control_wg);
+  PETScWrappers::FissionSource fission_source(
+      this->problem->fixed_source, this->problem->fission, solver_wg, 
+      dealii::PreconditionIdentity());
+  dealii::SolverControl control(100, 1e-8);
+  const int size = 
+      this->dof_handler.n_dofs() * this->quadrature.size() * num_groups;
+  std::vector<dealii::PETScWrappers::MPI::Vector> eigenvectors;
+  eigenvectors.emplace_back(MPI_COMM_WORLD, size, size);
+  std::vector<double> eigenvalues = {0.5};
+  TypeParam eigensolver(control);
+  eigensolver.solve(fission_source, eigenvalues, eigenvectors, 1);
+  EXPECT_NEAR(eigenvalues[0], 1, 1e-5);
 }
 
 }  // namespace
