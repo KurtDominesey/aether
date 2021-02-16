1faa01a939c7d098fb9d9433807586ecc5f50f65
Kurt Dominesey <domink@rpi.edu>
2021-02-16 16:44:40 -0500

    add subspace eigen interface (WIP)

---
+++ src/pgd/sn/subspace_eigen.h
@@ -0,0 +1,22 @@
+#ifndef AETHER_PGD_SN_SUBSPACE_EIGEN_H_
+#define AETHER_PGD_SN_SUBSPACE_EIGEN_H_
+
+#include <deal.II/lac/block_vector.h>
+
+#include "pgd/sn/inner_products.h"
+
+namespace aether::pgd::sn {
+
+class SubspaceEigen {
+ public:
+  double virtual step(
+      dealii::BlockVector<double> &modes,
+      const std::vector<std::vector<InnerProducts>> &coefficients) = 0;
+  void virtual get_inner_products(
+      const dealii::BlockVector<double> &modes,
+      std::vector<std::vector<InnerProducts>> &inner_products) = 0;
+};
+
+}
+
+#endif  // AETHER_PGD_SN_SUBSPACE_EIGEN_H_
