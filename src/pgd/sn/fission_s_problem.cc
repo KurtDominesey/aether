7d679e4ca3fdcc2a680e6d1e6527fc385af3f754
Kurt Dominesey <domink@rpi.edu>
2021-02-25 16:24:48 -0500

    add spatio-angular subspace PGD eigensolvers (WIP)

--- src/pgd/sn/fission_s_problem.cc
+++ src/pgd/sn/fission_s_problem.cc
@@ -52,6 +52,131 @@ void FissionSProblem<dim, qdim>::set_cross_sections(
   fission_s_gs.set_cross_sections(this->mgxs_pseudos);
 }
 
+template <int dim, int qdim>
+double FissionSProblem<dim, qdim>::step(
+    dealii::BlockVector<double> &modes,
+    const std::vector<std::vector<InnerProducts>> &coefficients,
+    const double shift) {
+  set_cross_sections(coefficients);
+  // if (shift == 0)
+  return step_power_shift(modes, shift);
+  // return step_gd(modes, shift);
+}
+
+template <int dim, int qdim>
+double FissionSProblem<dim, qdim>::step_gd(dealii::BlockVector<double> &modes,
+                                           const double shift) {
+  // wrap operators for use with petsc
+  const int block_size = modes.block(0).size();
+  aether::PETScWrappers::BlockWrapper fixed_source_s_petsc(
+      modes.n_blocks(), MPI_COMM_WORLD, block_size, block_size, 
+      this->fixed_source_s);
+  aether::PETScWrappers::BlockWrapper fission_s_petsc(
+      modes.n_blocks(), MPI_COMM_WORLD, block_size, block_size, fission_s);
+  aether::PETScWrappers::BlockWrapper fission_s_gs_petsc(
+      modes.n_blocks(), MPI_COMM_WORLD, block_size, block_size, fission_s_gs);
+  // get initial rayleigh quotient
+  dealii::BlockVector<double> ax(modes);
+  dealii::BlockVector<double> bx(modes);
+  this->fission_s.vmult(ax, modes);
+  this->fixed_source_s.vmult(bx, modes);
+  double rayleigh = (modes * ax) / (modes * bx);
+  std::cout << "rayleigh? " << rayleigh << "\n";
+  fission_s_gs.set_shift(rayleigh);  // shift preconditioner
+  // set up eigensolver
+  dealii::ReductionControl control(10, 1e-5, 1e-2);
+  dealii::SLEPcWrappers::SolverGeneralizedDavidson eigensolver(control);
+  eigensolver.set_target_eigenvalue(rayleigh);
+  // set intial guess
+  std::vector<dealii::PETScWrappers::MPI::Vector> eigenvectors;
+  eigenvectors.emplace_back(MPI_COMM_WORLD, modes.size(), modes.size());
+  for (int i = 0; i < modes.size(); ++i)
+    eigenvectors[0][i] = modes[i];
+  eigenvectors[0].compress(dealii::VectorOperation::insert);
+  eigensolver.set_initial_space(eigenvectors);
+  // set up preconditioner
+  dealii::SolverControl control_dummy(1, 0);  // dummy, solver ignores this
+  dealii::PETScWrappers::SolverPreOnly solver_pc(control_dummy);
+  aether::PETScWrappers::PreconditionerShell pc(fission_s_gs_petsc);
+  solver_pc.initialize(pc);
+  aether::SLEPcWrappers::TransformationPreconditioner stprecond(
+      MPI_COMM_WORLD, fission_s_gs_petsc);
+  stprecond.set_matrix_mode(ST_MATMODE_SHELL);
+  stprecond.set_solver(solver_pc);
+  eigensolver.set_transformation(stprecond);
+  // solve the eigenproblem
+  std::vector<double> eigenvalues;
+  eigensolver.solve(fission_s_petsc, fixed_source_s_petsc, eigenvalues, 
+                    eigenvectors);
+  double norm = modes.l2_norm();
+  for (int i = 0; i < modes.size(); ++i)
+    modes[i] = eigenvectors[0][i] * norm;
+  // compute residual
+  this->fixed_source_s.vmult(ax, modes);
+  fission_s.vmult(bx, modes);
+  ax.add(-eigenvalues[0], bx);
+  double residual = ax.l2_norm() / modes.l2_norm();
+  std::cout << "spatio-angular k: " << eigenvalues[0] << "\n";
+  std::cout << "spatio-angular residual: " << residual << "\n";
+  return residual;
+}
+
+template <int dim, int qdim>
+double FissionSProblem<dim, qdim>::step_power_shift(
+    dealii::BlockVector<double> &modes, const double shift) {
+  // get initial rayleigh quotient
+  dealii::BlockVector<double> ax(modes);
+  dealii::BlockVector<double> tmp(modes);  // temporary vector, bx here
+  fission_s.vmult(ax, modes);
+  this->fixed_source_s.vmult(tmp, modes);
+  double rayleigh = (modes * ax) / (modes * tmp);
+  std::cout << "rayleigh? " << rayleigh << "\n";
+  // one shifted power iteration
+  dealii::IterationNumberControl control(10, 1e-10);
+  using AdditionalData = 
+      dealii::SolverGMRES<dealii::BlockVector<double>>::AdditionalData;
+  dealii::SolverRichardson<dealii::BlockVector<double>> solver(control/*,
+      AdditionalData(1.2)*/);
+  double shift_or_rayleigh = shift == 0 ? rayleigh : shift;
+  if (true) {
+    tmp = modes;
+    for (int i = 0; i < 1; ++i) {
+      solver.solve(this->fixed_source_s, modes, ax, this->fixed_source_s_gs);
+      // modes.add(-shift_or_rayleigh, tmp);
+      fission_s.vmult(ax, modes);
+      modes /= this->l2_norm(modes);
+      // modes /= modes.l2_norm();
+      // modes /= shift == 0 ? rayleigh : shift;
+      // modes /= rayleigh;
+    }
+  } else {  // sinvert
+    shift_or_rayleigh += 5e-3;
+    shift_or_rayleigh = 1 / shift_or_rayleigh;
+    ShiftedS<dim, qdim> shifted(fission_s, this->fixed_source_s);
+    shifted.shift = shift_or_rayleigh;
+    fission_s_gs.set_shift(shift_or_rayleigh);
+    solver.solve(shifted, modes, tmp, fission_s_gs);
+  }
+  // modes /= modes.l2_norm();
+  // compute new rayleigh quotient
+  fission_s.vmult(ax, modes);
+  this->fixed_source_s.vmult(tmp, modes);
+  double rayleigh_new = (modes * ax) / (modes * tmp);
+  std::cout << "new rayleigh: " << rayleigh_new << "\n";
+  // compute residual
+  ax.add(-(shift == 0 ? rayleigh : shift), tmp);
+  double residual = ax.l2_norm(); // / modes.l2_norm();
+  std::cout << "spatio-angular residual: " << residual << "\n";
+  return residual;
+}
+
+template <int dim, int qdim>
+void FissionSProblem<dim, qdim>::get_inner_products(
+    const dealii::BlockVector<double> &modes,
+    std::vector<std::vector<InnerProducts>> &inner_products) {
+  this->fixed_source_s.get_inner_products_lhs(inner_products, modes);
+}
+
 template class FissionSProblem<1>;
 template class FissionSProblem<2>;
 template class FissionSProblem<3>;
--- src/pgd/sn/fission_s_problem.h
+++ src/pgd/sn/fission_s_problem.h
@@ -1,14 +1,26 @@
 #ifndef AETHER_PGD_SN_FISSION_S_PROBLEM_H_
 #define AETHER_PGD_SN_FISSION_S_PROBLEM_H_
 
+#include <deal.II/lac/block_vector.h>
+#include <deal.II/lac/petsc_vector.h>
+#include <deal.II/lac/petsc_solver.h>
+#include <deal.II/lac/slepc_solver.h>
+
+#include "base/petsc_block_wrapper.h"
+#include "base/petsc_precondition_shell.h"
+#include "base/slepc_transformation_preconditioner.h"
+
 #include "pgd/sn/fixed_source_s_problem.h"
 #include "pgd/sn/fission_s.h"
 #include "pgd/sn/fission_s_gs.h"
+#include "pgd/sn/subspace_eigen.h"
+#include "pgd/sn/shifted_s.h"
 
 namespace aether::pgd::sn {
 
 template <int dim, int qdim = dim == 1 ? 1 : 2>
-class FissionSProblem : public FixedSourceSProblem<dim, qdim> {
+class FissionSProblem : public FixedSourceSProblem<dim, qdim>, 
+                        public SubspaceEigen {
  public:
   /**
    * Constructor.
@@ -27,7 +39,32 @@ class FissionSProblem : public FixedSourceSProblem<dim, qdim> {
   void set_cross_sections(
       const std::vector<std::vector<InnerProducts>> &coefficients);
 
+  /**
+   * Update the modes.
+   */
+  double step(dealii::BlockVector<double> &modes,
+              const std::vector<std::vector<InnerProducts>> &coefficients,
+              const double shift);
+
+  /**
+   * Compute inner products.
+   */
+  void get_inner_products(
+      const dealii::BlockVector<double> &modes,
+      std::vector<std::vector<InnerProducts>> &inner_products);
+
  protected:
+  /**
+   * Update the modes using Generalized Davidson after setting coefficients.
+   */
+  double step_gd(dealii::BlockVector<double> &modes, const double shift);
+
+  /**
+   * Update the modes using Shifted Power after setting coefficients.
+   */
+  double step_power_shift(dealii::BlockVector<double> &modes, 
+                          const double shift);
+
   std::vector<std::vector<aether::sn::Emission<dim>>> emission;
   std::vector<std::vector<aether::sn::Production<dim>>> production;
 
