480820de30863a1a21577d06891fb6242504a11c
Kurt Dominesey <domink@rpi.edu>
2021-11-17 13:48:53 -0500

    add block Gauss Seidel test for growing LU precon.

--- src/base/tests/precondition_growing_lu_test.cc
+++ src/base/tests/precondition_growing_lu_test.cc
@@ -102,6 +102,54 @@ TEST_F(PreconditionBlockGrowingLUTest, Jacobi) {
   }
 }
 
+TEST_F(PreconditionBlockGrowingLUTest, GaussSeidel) {
+  dealii::LAPACKFullMatrix_<double> c;
+  int block_size = 5;
+  v.reinit(block_size);
+  av.reinit(block_size);
+  iv.reinit(block_size);
+  a.reinit(block_size);
+  for (int i = 0; i < block_size; ++i) {
+    a(i, i) = i + 1;
+    v = std::pow(i, 2);
+  }
+  a.vmult(av, v);
+  a_jac.initialize(a);
+  a_jac.matrix = &a;
+  a_jac.vmult(iv, av);
+  for (int i = 0; i < v.size(); ++i)
+    EXPECT_NEAR(iv[i], v[i], 1e-14);
+  for (int num_blocks = 1; num_blocks <= 3; ++num_blocks) {
+    int size = num_blocks * block_size;
+    d.reinit(block_size);
+    c.reinit(block_size, size);
+    a.grow_or_shrink(a.m()+block_size);
+    v.grow_or_shrink(v.size()+block_size);
+    av.reinit(v.size());
+    iv.reinit(v.size());
+    int last = num_blocks * block_size;
+    for (int bl = 0; bl <= num_blocks; ++bl) {
+      int off = bl * block_size;
+      for (int i = 0; i < block_size; ++i) {
+        for (int j = 0; j < block_size; ++j) {
+          if (bl < num_blocks) {
+            c(i, off+j) = -(bl+1) * (i+j+1);
+            a(last+i, off+j) = c(i, off+j);
+          } else {
+            d(i, j) = std::pow(i+j+2, 2);
+            a(last+i, last+j) = d(i, j);
+          }
+        }
+      }
+    }
+    a.print_formatted(std::cout);
+    a.vmult(av, v);
+    a_jac.initialize(d);
+    a_jac.vmult(iv, av);
+    for (int i = 0; i < v.size(); ++i)
+      EXPECT_NEAR(iv[i], v[i], 1e-14);
+  }
+}
 
 }  // namespace
 
