e486b09ac71816508e1d0897aacb933eb696c7bc
Kurt Dominesey <domink@rpi.edu>
2022-12-11 20:33:32 -0500

    fix typo in mgxs of unrodded Takeda LWR

--- examples/takeda/lwr/mesh.h
+++ examples/takeda/lwr/mesh.h
@@ -213,7 +213,7 @@ aether::Mgxs Lwr::create_mgxs() const {
   mgxs.scatter[0][0] = {1.92423e-1, 1.93446e-1, is_rodded ? 6.77241e-2 : 1.27700e-2};
   mgxs.scatter[0][1] = {0.0, 0.0, 0.0};
   mgxs.scatter[1][0] = {2.28253e-2, 5.65042e-2, is_rodded ? 6.45461e-5 : 2.40997e-5};
-  mgxs.scatter[1][1] = {8.80439e-1, 1.62452,    3.52358e-2, 1.07387e-2};
+  mgxs.scatter[1][1] = {8.80439e-1, 1.62452,    is_rodded ? 3.52358e-2 : 1.07387e-2};
   mgxs.nu_fission[0] = {9.09319e-3, 0.0, 0.0};
   mgxs.nu_fission[1] = {2.90183e-1, 0.0, 0.0};
   mgxs.chi[0] = {1.0, 0.0, 0.0};
